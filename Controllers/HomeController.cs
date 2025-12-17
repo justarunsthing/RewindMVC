dff2dd4afb191cef9d10f41b5a9df5c8904a0616
justarunsthing <justarunsthing@outlook.com>
2025-12-17 08:16:02 +0000

    Removed Privacy action & view

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,11 +11,6 @@ namespace RewindMVC.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {

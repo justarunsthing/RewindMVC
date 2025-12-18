e8dc03610d2b8ba42aac96a385166eb0427af448
justarunsthing <justarunsthing@outlook.com>
2025-12-18 08:23:15 +0000

    Added Rewind model

---
+++ Models/Rewind.cs
@@ -0,0 +1,9 @@
+﻿namespace RewindMVC.Models
+{
+    public class Rewind
+    {
+        public string? Input { get; set; }
+        public string? Result { get; set; }
+        public string? Message { get; set; }
+    }
+}

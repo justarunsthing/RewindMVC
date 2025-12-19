ee978099c62cbd0e13128a7b53b0cea5183bedff
justarunsthing <justarunsthing@outlook.com>
2025-12-19 08:29:04 +0000

    Added App post action

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,6 +18,27 @@ namespace RewindMVC.Controllers
             return View(model);
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult App(Rewind model)
+        {
+            if (!string.IsNullOrEmpty(model.Input))
+            {
+                var userInput = model.Input.ToLower().Replace(" ", "");
+                var reversedInput = string.Empty;
+
+                for (int i = userInput.Length - 1; i >= 0; i--)
+                {
+                    reversedInput += userInput[i];
+                }
+
+                model.Result = reversedInput;
+                model.Message = $"The reversed string is \"{model.Result}\"";
+            }
+
+            return View(model);
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
